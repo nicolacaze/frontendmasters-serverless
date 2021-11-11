72cb38f982a67f5f5350afb5378a99c17eacc415
Nicolas <nicolas.lacaze@gmail.com>
2021-11-11 14:26:19 +0100

    Add authentication flow with Netlify identity and authorization on running specific serverless functions

--- functions/add-movie.js
+++ functions/add-movie.js
@@ -1,7 +1,18 @@
 const { queryHasura } = require("./utils/hasura");
 
-exports.handler = async (event) => {
+exports.handler = async (event, context) => {
   const { id, title, tagline, poster } = JSON.parse(event.body);
+
+  const { user } = context.clientContext;
+  const isLoggedIn = user && user.app_metadata;
+  const roles = user.app_metadata.roles || [];
+
+  if(!isLoggedIn || !roles.includes("admin")) {
+    return {
+      statusCode: 401,
+      body: "Unatuhorized"
+    }
+  }
 
   const result = await queryHasura({
     query: `
